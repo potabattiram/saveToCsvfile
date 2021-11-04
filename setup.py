db1d92f5234d3b4994479adec8836eeaedeb7c2b
Balram <webdevforindia@gmail.com>
2021-11-04 11:07:17 +0530

    V1.0.0

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import pathlib
+
+#here = pathlib.Path(__file__).parent
+#README = (here / "README.md").read_text()
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='saveToCsv', 
+    version='1.0.0',
+    description='A Python module that helps to save data into csv file locally', 
+    author='Ram Potabatti',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url='https://github.com/potabattiram/saveToCsv',
+    author_email='potabattiram@gmail.com',  
+    classifiers=[  
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System' :: 'OS Independent',
+    ],
+    keywords='savetocsv, data in backend, development', 
+    packages=['saveToCsv'], 
+    python_requires='>=3.6, <4',
+    install_requires=['pandas'],  
+    include_package_data = True,
+  
+)
